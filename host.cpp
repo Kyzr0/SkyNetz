0e3a67f699c24911492619b4dfbb3122cffc0ea1
Kyzr0 <kylenetz@gmail.com>
2017-09-16 14:23:20 +0930

    Create host.cpp

---
+++ host.cpp
@@ -0,0 +1,16 @@
+#include "person.h"
+#include "host.h"
+#include <iostream>
+using namespace std;
+
+host::host():person(){
+
+}
+
+host::host(string hname):person(hname){
+
+}
+
+host::~host(){
+
+}
